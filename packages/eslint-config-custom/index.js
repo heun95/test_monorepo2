5b72734a3691b7a25807c43f0cf93b79a354eb81
Kang Jeonghwan <happykiwi90@gmail.com>
2023-04-11 14:10:08 +0900

    app1, app2 추가

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,20 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+		"next",
+		"turbo",
+		"prettier",
+	],
+	rules: {
+		"@next/next/no-html-link-for-pages": "off",
+	},
+	plugins: ["@typescript-eslint"],
+	parser: "@typescript-eslint/parser",
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };

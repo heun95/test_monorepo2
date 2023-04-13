b868f4b0148b31df50b8329e51c931214f9f42b8
Kang Jeonghwan <happykiwi90@gmail.com>
2023-04-13 10:46:44 +0900

    all package changed

--- packages/ui/dist/index.mjs
+++ packages/ui/dist/index.mjs
@@ -1,7 +1,7 @@
 // src/Button.tsx
 import { jsx } from "react/jsx-runtime";
 var Button = () => {
-  return /* @__PURE__ */ jsx("button", { children: "Boop2" });
+  return /* @__PURE__ */ jsx("button", { children: "button changed retry" });
 };
 export {
   Button

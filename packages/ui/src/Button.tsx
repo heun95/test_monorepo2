a935d567b1302472283b4243c9cdf42504920fcd
Kang Jeonghwan <happykiwi90@gmail.com>
2023-12-13 15:08:38 +0900

    haha after double

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 export const Button = () => {
 	console.log("ha ha");
 	console.log("ha ha");
+	console.log("ha ha");
 
 	return <button>button changed </button>;
 };
